83813b2d455f6fab3bf4419399abf50b85d20131
Jonathan Otto <jonathan.otto@gmail.com>
2016-12-18 16:33:14 -0800

    Add eslint, babel minify, and include dist

--- monkey-patch.js
+++ monkey-patch.js
@@ -1,7 +1,8 @@
-function ajaxMonkeyPatch(window, cachedResponses) {
+// eslint-disable-next-line no-unused-vars
+function ajaxMonkeyPatch (window, cachedResponses) {
   // https://coolaj86.com/articles/base64-unicode-utf-8-javascript-and-you/
-  function b64ToUtf8(b64) {
-    var binstr = atob(b64);
+  function b64ToUtf8 (b64) {
+    var binstr = window.atob(b64);
     var escstr = binstr.replace(/(.)/g, function (m, p) {
       var code = p.charCodeAt(0).toString(16).toUpperCase();
       if (code.length < 2) {
@@ -13,36 +14,35 @@ function ajaxMonkeyPatch(window, cachedResponses) {
   }
   try {
     cachedResponses = JSON.parse(b64ToUtf8(cachedResponses));
-  } catch(error) {
+  } catch (error) {
     return;
   }
 
   window.pcCachedResponses = cachedResponses;
-  var xhrProto = XMLHttpRequest.prototype,
-  origOpen = xhrProto.open;
-  xhrProto.open = function (method, url) {
-    this._url = url;
+  var origOpen = window.XMLHttpRequest.prototype.open;
+  window.XMLHttpRequest.prototype.open = function (method, url) {
+    this._precloudurl = url;
 
     // normalize relative paths as absolute paths on same origin (because that's what the server does)
-    if (this._url && this._url.startsWith('/')) this._url = window.location.origin + this._url;
+    if (this._precloudurl && this._precloudurl.startsWith('/')) this._precloudurl = window.location.origin + this._precloudurl;
 
     return origOpen.apply(this, arguments);
   };
 
-  var send = XMLHttpRequest.prototype.send;
-  XMLHttpRequest.prototype.send = function() {
-    if (cachedResponses[this._url]) {
+  var send = window.XMLHttpRequest.prototype.send;
+  window.XMLHttpRequest.prototype.send = function () {
+    if (cachedResponses[this._precloudurl]) {
       // http://stackoverflow.com/questions/26447335/how-can-i-modify-the-xmlhttprequest-responsetext-received-by-another-function
       Object.defineProperty(this, 'responseText', {writable: true});
       Object.defineProperty(this, 'readyState', {writable: true});
       Object.defineProperty(this, 'status', {writable: true});
-      this.readyState = 4
-      this.status = 200
-      this.responseText = cachedResponses[this._url];
-      delete cachedResponses[this._url];
+      this.readyState = 4;
+      this.status = 200;
+      this.responseText = cachedResponses[this._precloudurl];
+      delete cachedResponses[this._precloudurl];
       return this.onreadystatechange();
     } else {
       return send.apply(this, arguments);
     }
-  }
+  };
 }
