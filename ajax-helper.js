7f9cc627cde21741ade2606a785fa61d85d9df8e
Jonathan Otto <jonathan.otto@gmail.com>
2016-12-18 21:51:32 -0800

    Add support for XHR event listeners

--- ajax-helper.js
+++ ajax-helper.js
@@ -1,13 +1,38 @@
 
 // https://plainjs.com/javascript/ajax/send-ajax-get-and-post-requests-47/
 
-function getAjax(url, success) {
+function getAjax(url, cb) {
   var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
   xhr.open('GET', url);
   xhr.onreadystatechange = function() {
-      if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+      if (xhr.readyState>3 && xhr.status==200) cb(xhr.responseText);
   };
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
   xhr.send();
+
   return xhr;
 }
+
+function getAjaxWithEventListener(url, cb) {
+  var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+  xhr.open('GET', url);
+
+  var ensureAll3ListenersAreCalled = function(listenerName) {
+    xhr[listenerName] = true;
+    if (xhr.__onreadystatechange && xhr.__readystatechangeEvent && xhr.__loadEvent) cb(xhr.responseText);
+  }
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState>3 && this.status==200) ensureAll3ListenersAreCalled('__onreadystatechange');
+  };
+  xhr.addEventListener('readystatechange', function() {
+    if (this.readyState>3 && this.status==200) ensureAll3ListenersAreCalled('__readystatechangeEvent');
+  });
+  xhr.addEventListener('load', function() {
+    if (this.readyState>3 && this.status==200) ensureAll3ListenersAreCalled('__loadEvent');
+  });
+  xhr.send();
+
+  return xhr;
+}
+
+window.getAjax = getAjaxWithEventListener;
--- monkey-patch.js
+++ monkey-patch.js
@@ -18,7 +18,10 @@ function ajaxMonkeyPatch (window, cachedResponses) {
     return;
   }
 
+  // for debugging purposes only
   window.pcCachedResponses = cachedResponses;
+
+  // keep track of the URL
   var origOpen = window.XMLHttpRequest.prototype.open;
   window.XMLHttpRequest.prototype.open = function (method, url) {
     this._precloudurl = url;
@@ -29,18 +32,30 @@ function ajaxMonkeyPatch (window, cachedResponses) {
     return origOpen.apply(this, arguments);
   };
 
-  var send = window.XMLHttpRequest.prototype.send;
+  // immediately call the callbacks/listeners on send with the stubbed results
+  var origSend = window.XMLHttpRequest.prototype.send;
   window.XMLHttpRequest.prototype.send = function () {
     if (cachedResponses[this._precloudurl]) {
-      // http://stackoverflow.com/questions/26447335/how-can-i-modify-the-xmlhttprequest-responsetext-received-by-another-function
       var response = cachedResponses[this._precloudurl];
+      delete cachedResponses[this._precloudurl];
+
+      // http://stackoverflow.com/questions/26447335/how-can-i-modify-the-xmlhttprequest-responsetext-received-by-another-function
+      // see above link with one exception: to get IE11 to work, use getters, not values
       Object.defineProperty(this, 'responseText', {get: function () { return response; }});
       Object.defineProperty(this, 'readyState', {get: function () { return 4; }});
       Object.defineProperty(this, 'status', {get: function () { return 200; }});
-      delete cachedResponses[this._precloudurl];
-      return this.onreadystatechange();
+
+      // in the order that chrome fires them: (onreadystatechange cb, readystatechange listener, load listener)
+      if (this.onreadystatechange) this.onreadystatechange();
+      if (this.dispatchEvent && document.createEvent) {
+        ['readystatechange', 'load'].forEach(function (eventName) {
+          var ev = document.createEvent('Event');
+          ev.initEvent(eventName, true, true);
+          this.dispatchEvent(ev);
+        }, this);
+      }
     } else {
-      return send.apply(this, arguments);
+      return origSend.apply(this, arguments);
     }
   };
 }
