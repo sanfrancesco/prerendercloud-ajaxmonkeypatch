89296c48d38c0d8ce96cd01094fa0fe00d74e2af
Jonathan Otto <jonathan.otto@gmail.com>
2017-02-13 17:35:40 -0800

    Add ajax-bypass

---
+++ ajax-bypass.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line no-unused-vars
+function ajaxMonkeyPatchForBypass (window) {
+  if (!String.prototype.startsWith) {
+    // eslint-disable-next-line no-extend-native
+    String.prototype.startsWith = function (searchString, position) {
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+    };
+  }
+
+  var open = window.XMLHttpRequest.prototype.open;
+  window.XMLHttpRequest.prototype.open = function () {
+    var url = arguments[1];
+    var ret = open.apply(this, arguments);
+    if (url.startsWith(window.location.origin) || url.startsWith('/')) {
+      this.setRequestHeader('x-prerendered', 'true');
+    }
+    return ret;
+  };
+
+  var realFetch = window.fetch;
+  window.fetch = function (urlOrReq, init) {
+    if (urlOrReq && urlOrReq.headers) {
+      if ((urlOrReq && urlOrReq.url) && (urlOrReq.url.startsWith('/') || urlOrReq.url.startsWith(window.location.origin))) {
+        urlOrReq.headers.set('x-prerendered', true);
+      }
+      return realFetch(urlOrReq, init || urlOrReq);
+    } else {
+      if ((urlOrReq) && (urlOrReq.startsWith('/') || urlOrReq.startsWith(window.location.origin))) {
+        init = init || {};
+        init.headers = init.headers || {};
+        init.headers['x-prerendered'] = 'true';
+      }
+      return realFetch(urlOrReq, init);
+    }
+  };
+}
