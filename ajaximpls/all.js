8423504ae4b59dd7233ee9c338d4599ad5c8fd41
Jonathan Otto <jonathan.otto@gmail.com>
2017-05-23 13:01:26 -0700

    Add window.fetch support to ajax-preload

--- ajaximpls/all.js
+++ ajaximpls/all.js
@@ -1,10 +1,18 @@
 var ajaxImpls = {
   onReadyStatechange: ajaxOnReadyStateChange,
-  onReadyStatechangePost: function(params, url, cb) { return ajaxOnReadyStateChange(url, cb, { method: 'POST', params: params })},
+  onReadyStatechangePost: function(params, url, cb) {
+    return ajaxOnReadyStateChange(url, cb, { method: "POST", params: params });
+  },
   listener: ajaxListener,
   angular: ajaxAngular,
   reqwest: reqwest,
   jQuery: function(url, cb) {
-    jQuery.ajax(url, { method: 'GET' }).then(cb)
+    jQuery.ajax(url, { method: "GET" }).then(cb);
+  },
+  fetch: function(url, cb) {
+    return window.fetch(url).then(res => res.json().then(cb));
+  },
+  fetchRequestObj: function(url, cb) {
+    return window.fetch(new Request(url)).then(res => res.json().then(cb));
   }
-}
+};
