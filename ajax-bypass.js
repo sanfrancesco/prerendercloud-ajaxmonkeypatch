5c3e4cb283ee06249e387fd045589b02edca0306
Jonathan Otto <jonathan.otto@gmail.com>
2017-05-30 16:52:00 -0700

    Add pathsToIgnore param to Ajax-Bypass
    - this allows the prerender server to pass paths that look like
      innocent relative paths, but are really redirects to other hosts, and
      thus prevent Ajax-Bypass from adding x-prerendered header
    - TL;DR: fixes:
      - Request header field x-prerendered is not allowed by
        Access-Control-Allow-Headers in preflight response

--- ajax-bypass.js
+++ ajax-bypass.js
@@ -1,5 +1,26 @@
 // eslint-disable-next-line no-unused-vars
-function ajaxMonkeyPatchForBypass (window) {
+function ajaxMonkeyPatchForBypass (window, inputPathsToIgnore) {
+  if (!Array.prototype.indexOf) return;
+
+  // pathsToIgnore are what service.prerender.cloud detected as
+  // paths that redirect to another host, which means:
+  //   1. we won't need this header
+  //   2. leaving this header in would cause the following error for servers without proper CORS exceptions
+  //      Request header field x-prerendered is not allowed by Access-Control-Allow-Headers in preflight response.
+  //
+  var pathsToIgnore;
+  try {
+    if (!inputPathsToIgnore) {
+      pathsToIgnore = [];
+    } else {
+      pathsToIgnore = JSON.parse(inputPathsToIgnore);
+    }
+  } catch (err) {
+    return;
+  }
+
+  window.pcPathsToIgnore = pathsToIgnore;
+
   if (!String.prototype.startsWith) {
     // eslint-disable-next-line no-extend-native
     String.prototype.startsWith = function (searchString, position) {
@@ -8,11 +29,19 @@ function ajaxMonkeyPatchForBypass (window) {
     };
   }
 
+  var validUrlForBypass = function (url) {
+    var startsWithOrigin = url.startsWith(window.location.origin);
+    var relativePath = url.startsWith('/');
+    var isPathToIgnore = pathsToIgnore.indexOf(url) !== -1;
+
+    return (startsWithOrigin || relativePath) && !isPathToIgnore;
+  };
+
   var open = window.XMLHttpRequest.prototype.open;
   window.XMLHttpRequest.prototype.open = function () {
     var url = arguments[1];
     var ret = open.apply(this, arguments);
-    if (url.startsWith(window.location.origin) || url.startsWith('/')) {
+    if (url && validUrlForBypass(url)) {
       this.setRequestHeader('x-prerendered', 'true');
     }
     return ret;
@@ -22,12 +51,12 @@ function ajaxMonkeyPatchForBypass (window) {
     var realFetch = window.fetch;
     window.fetch = function (urlOrReq, init) {
       if (urlOrReq && urlOrReq.headers) {
-        if ((urlOrReq && urlOrReq.url) && (urlOrReq.url.startsWith('/') || urlOrReq.url.startsWith(window.location.origin))) {
+        if ((urlOrReq && urlOrReq.url) && validUrlForBypass(urlOrReq.url)) {
           urlOrReq.headers.set('x-prerendered', true);
         }
         return realFetch(urlOrReq, init || urlOrReq);
       } else {
-        if ((urlOrReq) && (urlOrReq.startsWith('/') || urlOrReq.startsWith(window.location.origin))) {
+        if (urlOrReq && validUrlForBypass(urlOrReq)) {
           init = init || {};
           init.headers = init.headers || {};
           init.headers['x-prerendered'] = 'true';
